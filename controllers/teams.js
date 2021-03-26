9cb45722d1bfa42996a9bb87f3dcd5b560866dc4
connor gerrits <cgerrits555@gmail.com>
2021-03-26 19:25:16 -0400

    Added a simple POST route and associated controller function to index.js and controllers/heroes.js, respectively.

--- controllers/teams.js
+++ controllers/teams.js
@@ -14,4 +14,21 @@ let getTeamById = (request, response) => {
         : response.send("No team with that id exists.")
 }
 
-module.exports = { getTeams, getTeamById }
+let saveNewTeam = (request, response) => {
+    let { id, location, mascot, abbreviation, conference, division } = request.body
+
+    if (!location || !mascot || !abbreviation || !conference || !division) {
+        return response.status(400).send('The data object you entered is invalid and may be missing data for some elements.')
+    }
+
+    else if (!id && location && mascot && abbreviation && conference && division) {
+        id = parseInt(teams[teams.length-1].id) + 1
+    }
+
+    const newTeam = { id, location, mascot, abbreviation, conference, division }
+    teams.push(newTeam)
+
+    return response.status(201).send(newTeam)
+}
+
+module.exports = { getTeams, getTeamById, saveNewTeam }
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
-const { getTeams, getTeamById } = require('./controllers/teams')
+const bodyParser = require('body-parser')
+const { getTeams, getTeamById, saveNewTeam } = require('./controllers/teams')
 
 const app = express()
 
 app.get('/teams', getTeams)
 
 app.get('/teams/:id', getTeamById)
+
+// app.use('body-parser')
+
+app.post('/teams', bodyParser.json(), saveNewTeam)
 
 app.all('*', (request, response) => {
     return response.status(404).send("There ain't nothin here!")
